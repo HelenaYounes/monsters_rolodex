9e6db2e7340bd2a697991155222a5bd1a4e78630
Héléna Younes <younesq.helena@gmail.com>
2023-03-24 03:12:14 +0100

    added class components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,52 @@
 import { Component } from 'react';
-import logo from './logo.svg';
+import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState(()=> {return {monsters: users}}));
+  }
+
+  onSearchChange = (e) =>{
+    const searchField = e.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField }
+    });
+  }
+
   render() {
+    const {monsters, searchField} = this.state;
+    const { onSearchChange } = this;
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+        <h1 className='app-title'>Monsters Rolodex</h1>
+        <SearchBox className="search-box-monsters" placeholder="search monster" onChangeHandler={onSearchChange} />
+        <CardList monsters={filteredMonsters} />
+        {
+          this.state.monsters.map(monster => {
+            return (
+              <h1 key={monster.id}>{monster.name}</h1>
+            )
+          })}
+        </div>
+      );
+    }
   }
-}
 
-export default App;
+  export default App;
