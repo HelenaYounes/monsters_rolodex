60c0adf238c7c209c8ccd192ebc3b8380458b895
Héléna Younes <younesq.helena@gmail.com>
2023-03-25 18:40:00 +0100

    added functional components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
-import { Component } from 'react';
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import SearchBox from './components/search-box/search-box';
+import CardList from './components/card-list/card-list';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+const App = () => {
+  const [monsters, setMonsters] = useState([]);
+  const [searchField, setSearchField] = useState('');
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => setMonsters(users));
+  },[]);
+
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter(monster => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+    setFilteredMonsters(newFilteredMonsters);
+  }, [monsters, searchField]);
+
+  const onSearchChange = (e) => {
+    const searchFieldString = e.target.value.toLocaleLowerCase();
+    setSearchField(searchFieldString);
+  };
+
+  return (
+    <div className='App'>
+      <h1 className='app-title'> Monsters Rolodex </h1>
+      <SearchBox
+        className='monsters-search-box'
+        onChangeHandler={onSearchChange}
+        placeholder='search monster' />
+      <CardList monsters={filteredMonsters}/>
       </div>
     );
   }
-}
 
-export default App;
+  export default App;
---
+++ src/components/card/card.jsx
@@ -0,0 +1,14 @@
+import './card.css';
+
+const Card = ({monster}) => {
+  const {id, name, email} = monster;
+  return(
+    <div className='card-container'>
+      <img  src={`https://robohash.org/${id}`} alt={`monster ${name}`}/>
+      <h2>{name}</h2>
+      <p>{email}</p>
+    </div>
+  );
+}
+
+export default Card;
